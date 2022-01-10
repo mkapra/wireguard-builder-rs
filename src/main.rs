5d4a9436283c37a1a0bbb5331547bad8531ab2fe
Maximilian Kapra <maximilian@kapra.de>
2022-01-10 12:48:46 +0100

    Basic structure with async-graphql and rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use async_graphql::{
+    http::{playground_source, GraphQLPlaygroundConfig},
+};
+use async_graphql_rocket::{GraphQLQuery, GraphQLRequest, GraphQLResponse};
+use rocket::{response::content, routes, State};
+
+mod schemas;
+use schemas::{GrahpQLSchema, create_schema};
+
+#[rocket::get("/")]
+fn graphql_playground() -> content::Html<String> {
+    content::Html(playground_source(GraphQLPlaygroundConfig::new("/graphql")))
+}
+
+#[rocket::get("/graphql?<query..>")]
+async fn graphql_query(schema: &State<GrahpQLSchema>, query: GraphQLQuery) -> GraphQLResponse {
+    query.execute(schema).await
+}
+
+#[rocket::post("/graphql", data = "<request>", format = "application/json")]
+async fn graphql_request(
+    schema: &State<GrahpQLSchema>,
+    request: GraphQLRequest,
+) -> GraphQLResponse {
+    request.execute(schema).await
+}
+
+#[rocket::launch]
+fn rocket() -> _ {
+    rocket::build().manage(create_schema()).mount(
+        "/",
+        routes![graphql_query, graphql_request, graphql_playground],
+    )
 }
---
+++ src/schemas.rs
@@ -0,0 +1,17 @@
+use async_graphql::*;
+
+pub type GrahpQLSchema = Schema<QueryRoot, EmptyMutation, EmptySubscription>;
+
+pub fn create_schema() -> GrahpQLSchema {
+    Schema::build(QueryRoot, EmptyMutation, EmptySubscription).finish()
+}
+
+pub struct QueryRoot;
+
+#[Object]
+impl QueryRoot {
+    /// Says hello
+    async fn say_hello(&self) -> String {
+      "Hello World!".to_owned()
+    }
+}
