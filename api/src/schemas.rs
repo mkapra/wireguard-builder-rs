6adf8d00b6c292fc01521057c63bdaaa5c62f92c
Maximilian Kapra <maximilian@kapra.de>
2022-01-11 11:56:54 +0100

    Finish genearateKeypair implementation
    * Move generation to keypair schema

--- api/src/schemas.rs
+++ api/src/schemas.rs
@@ -3,8 +3,6 @@ use diesel::{
     r2d2::{ConnectionManager, Pool, PooledConnection},
     PgConnection,
 };
-use std::process::{Command, Stdio};
-use std::{io::Write, str};
 
 mod keypair;
 use keypair::{create_keypair, Keypair};
@@ -50,33 +48,7 @@ impl Mutation {
             .get()
             .expect("Recieved no connection from pool");
 
-        // Generate private key
-        let command_privkey = Command::new("wg")
-            .arg("genkey")
-            .output()
-            .expect("Failed to execute command")
-            .stdout;
-        let priv_key = str::from_utf8(&command_privkey)
-            .expect("Could not parse private key")
-            .replace("\n", "");
-
-        // Generate public key
-        let pubkey_command = Command::new("wg")
-            .arg("pubkey")
-            .stdin(Stdio::piped())
-            .spawn()
-            .expect("Failed to generate public key");
-        let stdin = pubkey_command
-            .stdin
-            .as_ref()
-            .unwrap()
-            .write_all(priv_key.as_bytes());
-        if let Err(e) = stdin {
-            panic!("Could not read: {}", e);
-        }
-        // let pubkey_output = pubkey_command.wait_with_output().expect("Did not get a response from wg pubkey");
-        // let pub_key = String::from_utf8(pubkey_output.stdout).expect("Could not parse public key");
-
-        create_keypair(&connection, &priv_key, &priv_key)
+        let (priv_key, pub_key) = Keypair::generate_keypair();
+        create_keypair(&connection, &priv_key, &pub_key)
     }
 }
--- api/src/schemas/keypair.rs
+++ api/src/schemas/keypair.rs
@@ -1,3 +1,5 @@
+use std::process::{Command, Stdio};
+use std::{io::Write, str};
 use async_graphql::*;
 use diesel::{Insertable, Queryable};
 
@@ -9,31 +11,69 @@ use crate::schemas::SingleConnection;
 pub struct Keypair {
     /// The id of the keypair
     id: i32,
-    /// The private key of the keypair
-    private_key: String,
     /// The public key of the keypair
     public_key: String,
+    /// The private key of the keypair
+    private_key: String,
 }
 
 #[derive(Insertable)]
 #[table_name = "keypairs"]
 pub struct NewKeypair<'a> {
-    pub private_key: &'a str,
     pub public_key: &'a str,
+    pub private_key: &'a str,
 }
 
 pub fn create_keypair<'a>(
     connection: &SingleConnection,
-    private_key: &'a str,
     public_key: &'a str,
+    private_key: &'a str,
 ) -> Keypair {
     let new_keypair = NewKeypair {
-        private_key,
         public_key,
+        private_key,
     };
 
     diesel::insert_into(keypairs::table)
         .values(&new_keypair)
         .get_result(connection)
         .expect("Error saving generated keypair")
+}
+
+impl Keypair {
+    /// Generates a new keypair and returns it
+    pub fn generate_keypair() -> (String, String) {
+        // Generate private key
+        let command_privkey = Command::new("wg")
+            .arg("genkey")
+            .output()
+            .expect("Failed to execute command")
+            .stdout;
+        let priv_key = str::from_utf8(&command_privkey)
+            .expect("Could not parse private key")
+            .replace("\n", "");
+
+        // Generate public key
+        let pubkey_command = Command::new("wg")
+            .arg("pubkey")
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("Failed to generate public key");
+        let stdin = pubkey_command
+            .stdin
+            .as_ref()
+            .unwrap()
+            .write_all(priv_key.as_bytes());
+        if let Err(e) = stdin {
+            panic!("Could not read: {}", e);
+        }
+
+        let pubkey_output = pubkey_command.wait_with_output().expect("Did not get a response from wg pubkey");
+        let pub_key = str::from_utf8(&pubkey_output.stdout)
+            .expect("Could not parse public key")
+            .replace("\n", "");
+
+        (priv_key, pub_key)
+    }
 }
