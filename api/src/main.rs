864fdd4b015e144ccce76c9fa0620e9a6f70490b
Maximilian Kapra <maximilian@kapra.de>
2022-01-11 12:25:33 +0100

    Add documentation

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,21 +1,19 @@
 use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
 use async_graphql_rocket::{GraphQLQuery, GraphQLRequest, GraphQLResponse};
-use diesel::{
-    r2d2::{ConnectionManager, Pool},
-    PgConnection,
-};
+use diesel::r2d2::{ConnectionManager, Pool};
 use dotenv::dotenv;
 use rocket::{response::content, routes, State};
 use std::env;
 
 mod schemas;
-use schemas::{create_schema, GrahpQLSchema};
+use schemas::{create_schema, GrahpQLSchema, DatabaseConnection};
 
 #[macro_use]
 extern crate diesel;
 mod schema;
 
-fn establish_connection() -> Pool<ConnectionManager<PgConnection>> {
+/// Creates a new pool for connecting to the database
+fn establish_connection() -> DatabaseConnection {
     dotenv().ok();
 
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
@@ -23,16 +21,19 @@ fn establish_connection() -> Pool<ConnectionManager<PgConnection>> {
     Pool::new(manager).expect("Could not connect to database")
 }
 
+/// Playground for making graphql requests
 #[rocket::get("/")]
 fn graphql_playground() -> content::Html<String> {
     content::Html(playground_source(GraphQLPlaygroundConfig::new("/graphql")))
 }
 
+/// Endpoint for all graphql queries
 #[rocket::get("/graphql?<query..>")]
 async fn graphql_query(schema: &State<GrahpQLSchema>, query: GraphQLQuery) -> GraphQLResponse {
     query.execute(schema).await
 }
 
+/// Endpoint for all graphql requests
 #[rocket::post("/graphql", data = "<request>", format = "application/json")]
 async fn graphql_request(
     schema: &State<GrahpQLSchema>,
@@ -41,6 +42,7 @@ async fn graphql_request(
     request.execute(schema).await
 }
 
+/// Entrypoint of this binary crate that initializes the webserver
 #[rocket::launch]
 fn rocket() -> _ {
     rocket::build()
--- api/src/schemas.rs
+++ api/src/schemas.rs
@@ -1,3 +1,4 @@
+//! The GraphQL schema
 use async_graphql::{Context, *};
 use diesel::{
     r2d2::{ConnectionManager, Pool, PooledConnection},
@@ -10,16 +11,27 @@ use keypair::{create_keypair, Keypair};
 use crate::diesel::prelude::*;
 use crate::schema::keypairs::dsl::*;
 
+/// Represents the schema that is created by [`create_schema()`]
 pub type GrahpQLSchema = Schema<QueryRoot, Mutation, EmptySubscription>;
+/// Represents a pool of connections to the database
 pub type DatabaseConnection = Pool<ConnectionManager<PgConnection>>;
+/// Represents a single connection to the database
 pub type SingleConnection = PooledConnection<ConnectionManager<PgConnection>>;
 
-pub fn create_schema(connection: Pool<ConnectionManager<PgConnection>>) -> GrahpQLSchema {
+/// Creates a new schema with a connection pool for communicating with the database as context
+///
+/// # Arguments
+/// * `connection` - A pool for PostgreSQL connections
+///
+/// # Returns
+/// Returns a schema that can be used by the web framework
+pub fn create_schema(connection: DatabaseConnection) -> GrahpQLSchema {
     Schema::build(QueryRoot, Mutation, EmptySubscription)
         .data(connection)
         .finish()
 }
 
+/// The root of the Query type
 pub struct QueryRoot;
 
 #[Object]
@@ -36,6 +48,7 @@ impl QueryRoot {
     }
 }
 
+/// The root of the mutation type
 pub struct Mutation;
 
 #[Object]
