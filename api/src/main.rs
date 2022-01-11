1ddfd90bf783711516f29e94da4b5e02837364c9
Maximilian Kapra <maximilian@kapra.de>
2022-01-11 10:12:54 +0100

    Add database connection pool as context

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,11 +1,26 @@
+use dotenv::dotenv;
+use std::env;
 use async_graphql::{
     http::{playground_source, GraphQLPlaygroundConfig},
 };
 use async_graphql_rocket::{GraphQLQuery, GraphQLRequest, GraphQLResponse};
+use diesel::{PgConnection, r2d2::{ConnectionManager, Pool}};
 use rocket::{response::content, routes, State};
 
 mod schemas;
 use schemas::{GrahpQLSchema, create_schema};
+
+#[macro_use]
+extern crate diesel;
+mod schema;
+
+fn establish_connection() -> Pool<ConnectionManager<PgConnection>> {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let manager = ConnectionManager::new(&database_url);
+    Pool::new(manager).expect("Could not connect to database")
+}
 
 #[rocket::get("/")]
 fn graphql_playground() -> content::Html<String> {
@@ -27,7 +42,7 @@ async fn graphql_request(
 
 #[rocket::launch]
 fn rocket() -> _ {
-    rocket::build().manage(create_schema()).mount(
+    rocket::build().manage(create_schema(establish_connection())).mount(
         "/",
         routes![graphql_query, graphql_request, graphql_playground],
     )
