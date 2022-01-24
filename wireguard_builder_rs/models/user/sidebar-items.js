751ac481e9bf30f5fe560089509df4fc3da78858
mkapra <mkapra@users.noreply.github.com>
2022-01-24 10:24:42 +0000

    deploy: 79ec3b0919463a3902eb275fb0a4a0df84e0fbfc

--- wireguard_builder_rs/models/user/sidebar-items.js
+++ wireguard_builder_rs/models/user/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["_impl_insertable_for_graphqluser",""],["_impl_queryable_for_user",""]],"struct":[["GraphQLUser",""],["JwtUser",""],["User",""]]});
+initSidebarItems({"fn":[["_impl_identifiable_for_user",""],["_impl_insertable_for_graphqluser",""],["_impl_queryable_for_user",""]],"struct":[["GraphQLUser",""],["JwtUser",""],["User",""]]});
