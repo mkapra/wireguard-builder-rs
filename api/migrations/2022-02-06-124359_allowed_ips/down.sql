d6fb02c0d78f0b258f5eb6ef7f9cd1cb6eab64ec
Maximilian Kapra <maximilian@kapra.de>
2022-02-06 14:03:21 +0100

    Add migration for allowed ips

---
+++ api/migrations/2022-02-06-124359_allowed_ips/down.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS AllowedIpsClients;
+DROP TABLE IF EXISTS AllowedIps;
--- api/src/schema.rs
+++ api/src/schema.rs
@@ -1,4 +1,20 @@
 table! {
+    allowedips (id) {
+        id -> Int4,
+        ip -> Text,
+        subnetmask -> Int4,
+    }
+}
+
+table! {
+    allowedipsclients (id) {
+        id -> Int4,
+        ip_id -> Int4,
+        client_id -> Int4,
+    }
+}
+
+table! {
     clients (id) {
         id -> Int4,
         name -> Text,
@@ -67,6 +83,8 @@ table! {
     }
 }
 
+joinable!(allowedipsclients -> allowedips (ip_id));
+joinable!(allowedipsclients -> clients (client_id));
 joinable!(clients -> dns_servers (dns_server_id));
 joinable!(clients -> keypairs (keypair_id));
 joinable!(clients -> vpn_ip_addresses (vpn_ip_address_id));
@@ -75,6 +93,8 @@ joinable!(servers -> vpn_ip_addresses (vpn_ip_address_id));
 joinable!(vpn_ip_addresses -> vpn_networks (vpn_network_id));
 
 allow_tables_to_appear_in_same_query!(
+    allowedips,
+    allowedipsclients,
     clients,
     dns_servers,
     keypairs,
